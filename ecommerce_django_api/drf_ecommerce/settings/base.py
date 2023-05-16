ce657321d383e02e39b7c49bb1ac49e2dfb4429c
nagah <nagah.shinawy@gmail.com>
2023-05-16 10:30:03 +0300

    video#12 using env config

--- ecommerce_django_api/drf_ecommerce/settings/base.py
+++ ecommerce_django_api/drf_ecommerce/settings/base.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s)c(g*r$56y%0-6b@)u1&ax)#ju$%-0eaawa8zbp5+ru7iyr^_"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEVELOP = True
--- ecommerce_django_api/drf_ecommerce/settings/dev.py
+++ ecommerce_django_api/drf_ecommerce/settings/dev.py
@@ -1,3 +1,4 @@
+import os
 from .base import *
 
 
@@ -6,8 +7,12 @@ from .base import *
 
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DATABASE_NAME"),
+        'USER': os.environ.get("DATABASE_USER"),
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': os.environ.get("DATABASE_HOST"),
+        'PORT': 5432,
     }
 }
